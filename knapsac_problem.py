bc543c2aa808242966a14d183532303c639c4c67
MahamdiAmine <fm_mahamdi@esi.dz>
2018-10-20 14:07:17 -0400

    converting to python oriented object programming

---
+++ knapsac_problem.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3                                    #
+# -.- coding: utf-8 -.-                                   #
+# Author mahamdi amine                                    #
+# Github https://github.com/MahamdiAmine                  #
+###########################################################
+
+
+# NumPy is a library for the Python programming language, adding support for large,
+# multi-dimensional arrays and matrices, along with a large collection of high-level mathematical
+# functions to operate on these arrays.
+import numpy as np
+
+
+class Knapsac:
+
+# the knapSac function takes the n objects and returns the matrix of the max values
+    def knapSack(self,max_weight, weight, value):
+        n = len(value)
+        matrix = [[0 for x in range(max_weight + 1)] for x in range(n + 1)]
+        for i in range(n + 1):
+            for w in range(max_weight + 1):
+                if i == 0 or w == 0:
+                    matrix[i][w] = 0
+                elif weight[i - 1] <= w:
+                    matrix[i][w] = max(value[i - 1] + matrix[i - 1][w - weight[i - 1]], matrix[i - 1][w])
+                else:
+                    matrix[i][w] = matrix[i - 1][w]
+        return matrix
+
+
+# checking to see which items will go into our knapsack
+    def check_items(self,weight, matrix, max_weight):
+        # I'M NOT SURE THIS WILL WORK CORRECTLY!!!!!!!!!!
+        col = max_weight
+        packed = []
+        for i in range(len(weight) - 1, -1, -1):
+            if i == 0 and matrix[i][col] != 0:
+                packed.insert(0, i)
+            if matrix[i][col] != matrix[i - 1][col]:
+                packed.insert(0, i)
+                col -= weight[i]
+        packed = np.unique(packed)  # is it correct ??????
+        return packed
+# TODO the Graphical interface !!
---
+++ object.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3                                    #
+# -.- coding: utf-8 -.-                                   #
+# Author mahamdi amine                                    #
+# Github https://github.com/MahamdiAmine                  #
+###########################################################
+
+class Object:
+    packed=0
+    def __init__(self,id,weight,value):
+        self.id=id
+        self.weight=weight
+        self.value=value
+        packed=0
+    def is_packed(picked):
+        packed=picked
+    def get_weight(self):
+        return self.weight
+    def get_value(self):
+        return self.value
+    def get_id(self):
+        return self.id
+
